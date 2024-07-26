8618ee3f6a449aebf9c2943ae9b44914ad9683b1
Senrayan05 <asenrayan@gmail.com>
2024-07-26 16:05:00 +0530

    pushing  all files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import Assessment from "./Assessment";
 import Attendance from "./Attendance";
 import Classes from "./Classes";
+import Placements from "./Placements";
 
 function App() {
   return (
     <>
           Learn React
           <Classes/>
+          <Assessment/>
           <Attendance/>
-      
+          <Placements/>
     </>
   );
 }
--- src/Classes.jsx
+++ src/Classes.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Classes() {
   return (
-    <>Classes</>
+    <div>Classes</div>
   )
 }
